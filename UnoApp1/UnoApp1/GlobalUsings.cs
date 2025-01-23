788e0203dd6b6530da123e33e9f240d35fbd6101
Rhys <rhys@intelligentediting.com>
2025-01-23 13:17:53 +0000

    Add Word Loader and Uno repro

---
+++ UnoApp1/UnoApp1/GlobalUsings.cs
@@ -0,0 +1,10 @@
+﻿global using System.Collections.Immutable;
+global using CommunityToolkit.Mvvm.ComponentModel;
+global using CommunityToolkit.Mvvm.Input;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.Logging;
+global using Microsoft.Extensions.Options;
+global using UnoApp1.Models;
+global using UnoApp1.Presentation;
+global using ApplicationExecutionState = Windows.ApplicationModel.Activation.ApplicationExecutionState;
